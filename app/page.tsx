fe26c49b3e54687ab3d4acbee064127358d4692f
koljn2007 <koljn2007@gmail.com>
2026-03-18 18:04:16 +0300

    update site

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.setAttribute('data-telegram-login', 'SiteMelomanMusicBot');
     script.setAttribute('data-size', 'large');
-    script.setAttribute('data-onauth', 'window.onTelegramAuth(user)');
+    script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
 
     widgetRef.current.innerHTML = '';
